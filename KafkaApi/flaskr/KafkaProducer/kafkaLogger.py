61544f4ded8693b28b771f3c88417c32629387ab
rathinnn <rathinnnaiir77@gmail.com>
2021-05-08 16:52:09 +0530

    Fix Kafka Api Bug

--- KafkaApi/flaskr/KafkaProducer/kafkaLogger.py
+++ KafkaApi/flaskr/KafkaProducer/kafkaLogger.py
@@ -1,6 +1,7 @@
 from kafka import KafkaProducer
+import json
 producer = KafkaProducer(bootstrap_servers='localhost:9092',value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 def sendToTopic(app_json):
-    producer.send('TutorialTopic' , app_json)
+    producer.send('test2' , app_json)
     
--- KafkaApi/flaskr/app.py
+++ KafkaApi/flaskr/app.py
@@ -1,11 +1,8 @@
 from flask import Flask, render_template, request, url_for, redirect,jsonify
-#from KafkaProducer.kafkaLogger import sendToTopic
+from KafkaProducer.kafkaLogger import sendToTopic
 import logging
 import sys
 
-latsandlons = {
-    1:
-}
 app = Flask(__name__)
 
 #logging.basicConfig(level=logging.DEBUG)
@@ -13,10 +10,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route('/sendJsonlog',methods=['POST'])
+@app.route('/send',methods=['POST'])
 def getData():
     content = request.json
-    #sendToTopic(contenW)
+    sendToTopic(content)
     #app.logger.info(content)
     #app.logger.info('Hi')
     print(content)
